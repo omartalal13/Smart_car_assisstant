fc8a3050656f4df4182f6970721b15d3a59bf214
Omar Talal <omartalal13@yahoo.com>
2020-08-16 23:47:16 +0200

    Add dep install
    add the bash script to install the dependencies needed

---
+++ Install_dep.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install portaudio19-dev
+pip install pyttsx3 requests speechrecognition playsound wolframalpha gtts selenium pyAudio libespeak1 wikipedia google-cloud-storage numpy opencv-python pusher-push-notifications face_recognition
+pip install -r requirements_drowsiness.txt requirements_face.txt
+
+
